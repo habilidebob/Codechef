725bd8fdc7be6fa7817153d5a252b370754a1d41
Habilidebob <80491104+habilidebob@users.noreply.github.com>
2024-06-04 14:21:18 -0300

    correções colunas Cardapio.js

--- src/pages/Cardapio.js
+++ src/pages/Cardapio.js
@@ -18,30 +18,28 @@ function Cardapio() {
                 </Col>
             </Row>
             <Row className="mt-3">
-                <Col>
+                <Col md="3">
                     <ItemCardapio titulo="Hamburguer" img={hamburguer} descricao="Hamburguer caseiro suculento show 10 muito bom."/>
                 </Col>
-                <Col>
+                <Col md="3">
                     <ItemCardapio titulo="Torta Holandesa" img={tortah} descricao="Torta holandesa" />
                 </Col>
-                <Col>
+                <Col md="3">
                     <ItemCardapio titulo="Coxinha" img={coxinha} descricao="Bolinho de frango frito, tradicionalmente servido com maionese e ketchup." />
-                </Col>
-                <Col>
+                </Col >
+                <Col md="3">
                     <ItemCardapio titulo="T-Bone" img={tbone} descricao="Filé mignon com um osso em T no meio, geralmente grelhado ou assado." />
                 </Col>
-            </Row>
-            <Row className="mt-3">
-                <Col>
+                <Col md="3">
                     <ItemCardapio titulo="Kebab" img={kebab} descricao="Espetinho de carne, frango, vegetais ou frutos do mar, geralmente servido com molho de iogurte." />
                 </Col>
-                <Col>
+                <Col md="3">
                     <ItemCardapio titulo="Pastel" img={pastel} descricao="Um pastel é uma massa folhada frita recheada com vários ingredientes, como carne, frango, queijo, legumes ou camarão. É um prato popular em muitos países da América Latina e do Sul da Ásia." />
                 </Col>
-                <Col>
+                <Col md="3">
                     <ItemCardapio titulo="Picanha" img={picanha} descricao="Corte de carne bovina suculento e saboroso, perfeito para um churrasco ou uma refeição especial." />
                 </Col>
-                <Col>
+                <Col md="3">
                     <ItemCardapio titulo="Pudim" img={pudim} descricao="Sobremesa doce e cremosa, perfeita para terminar uma refeição com chave de ouro." />
                 </Col>
             </Row>
